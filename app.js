a40cf007eb2ba96742d5665757c0acfa908e25db
Kemal Emre Erginkoç <keerginkoc@gmail.com>
2022-10-03 18:46:29 +0300

    Revised

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const containerEl = document.querySelector(".container");
 
-for (let index = 0; index < 25; index++) {
+for (let index = 0; index < 30; index++) {
     const colorContainerEl = document.createElement("div");
     colorContainerEl.classList.add("color-container");
     containerEl.appendChild(colorContainerEl);
